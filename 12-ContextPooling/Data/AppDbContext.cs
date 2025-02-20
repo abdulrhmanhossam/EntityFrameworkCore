d5a36c5f9d9008acec51cce58570ef38d7a2a5e5
abdulrahman <abdulrahmn398@outlook.com>
2025-02-20 18:17:13 +0200

    Add ContextPooling project with EF Core integration
    This commit introduces a new project "12-ContextPooling" to the solution.
    It includes a .NET SDK project file with necessary configurations, an
    `appsettings.json` for database connection, and implements a
    dependency injection pattern in `Program.cs`.
    
    Additionally, it defines the `AppDbContext` and `Wallet` classes
    to manage wallet entities, utilizing context pooling for efficient
    database connection management.

---
+++ 12-ContextPooling/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+using _12_ContextPooling.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace _12_ContextPooling.Data;
+//  A DbContext instance represents a session with the database and can be used to
+//  query and save instances of your entities. DbContext is a combination of the
+//  Unit Of Work and Repository patterns.
+public class AppDbContext : DbContext
+{
+    // represent the collection of all entities
+    public DbSet<Wallet> Wallets { get; set; }
+
+
+    public AppDbContext(DbContextOptions options)
+        :base(options)
+    {
+        
+    }
+}
---
+++ 12-ContextPooling/Program.cs
@@ -0,0 +1,35 @@
+﻿using _12_ContextPooling.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+// build config file
+var config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+string? connectionString = config
+    .GetSection("ConnectionString").Value;
+
+// create obj from ServiceCollection to use
+var services = new ServiceCollection();
+
+// register appdbcontext as service and send options 
+// use context pool to change context state and reuse it after dispoise
+services.AddDbContextPool<AppDbContext>(options =>
+       options.UseSqlServer(connectionString));
+
+// return IOC contauiner
+IServiceProvider serviceProvider = services.BuildServiceProvider();
+
+// use appdbcontext from serviceProvider without create obj 
+// loose coupling
+using (var dbContext = serviceProvider.GetService<AppDbContext>())
+{
+    foreach (var wallet in dbContext!.Wallets)
+    {
+        Console.WriteLine(wallet);
+    }
+}
+
+
